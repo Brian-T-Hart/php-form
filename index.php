f7ff6c11a895bfab89a2a957b862001e8cbf4bf9
Brian-T-Hart <hart.briant@gmail.com>
2018-09-28 14:58:06 -0700

    changed an incorrect variable

--- index.php
+++ index.php
@@ -121,7 +121,7 @@
 			if (getenv('SERVER_NAME') === 'localhost') {
 				$db2 = mysqli_connect('localhost:3307', 'root', '', 'php');
 
-				$sql2 = 'SELECT * FROM messages ORDER BY id DESC';
+				$sql2 = 'SELECT * FROM messages ORDER BY id DESC LIMIT 10';
 
 				$result = mysqli_query($db2, $sql2);
 
@@ -132,7 +132,7 @@
 					);
 				}
 
-				mysqli_close($conn);
+				mysqli_close($db2);
 			}
 
 			else {
